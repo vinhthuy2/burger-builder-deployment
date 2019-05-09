fd6137c07907985f179cdc4ff161b891e9a7b859
vinhthuy2 <34227790+vinhthuy2@users.noreply.github.com>
2019-05-09 14:14:24 +0700

    Add files via upload

---
+++ precache-manifest.74f828ef50765c9f224f6cf59f595545.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "7997f2f9985fcfe1a800",
+    "url": "/reac-burger-builder/static/css/4.b4c28651.chunk.css"
+  },
+  {
+    "revision": "06aa00458ff2e562bbf9",
+    "url": "/reac-burger-builder/static/css/main.e9f3a487.chunk.css"
+  },
+  {
+    "revision": "c9a0c32e152034331277",
+    "url": "/reac-burger-builder/static/js/runtime~main.4fe32d73.js"
+  },
+  {
+    "revision": "56d3df5a9d2a05dfa430",
+    "url": "/reac-burger-builder/static/js/2.fa016389.chunk.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/reac-burger-builder/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "227071d0affd2a5c6d83",
+    "url": "/reac-burger-builder/static/js/3.a2ffd715.chunk.js"
+  },
+  {
+    "revision": "06aa00458ff2e562bbf9",
+    "url": "/reac-burger-builder/static/js/main.e649958e.chunk.js"
+  },
+  {
+    "revision": "7997f2f9985fcfe1a800",
+    "url": "/reac-burger-builder/static/js/4.2779a391.chunk.js"
+  },
+  {
+    "revision": "bb78775113cfd63c712d",
+    "url": "/reac-burger-builder/static/js/5.5c03252c.chunk.js"
+  },
+  {
+    "revision": "bb78775113cfd63c712d",
+    "url": "/reac-burger-builder/static/css/5.03aa874a.chunk.css"
+  },
+  {
+    "revision": "227071d0affd2a5c6d83",
+    "url": "/reac-burger-builder/static/css/3.3d37f730.chunk.css"
+  },
+  {
+    "revision": "2cb5bb281416a264810bdb084d44c243",
+    "url": "/reac-burger-builder/index.html"
+  }
+];
