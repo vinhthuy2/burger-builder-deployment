c1f93705ec7dc4ed911fec2ad9dade0b1249a18c
vinhthuy2 <34227790+vinhthuy2@users.noreply.github.com>
2019-05-09 14:36:54 +0700

    Add files via upload

---
+++ precache-manifest.543d922a9d4cc2c7962a14fcee5030bc.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "7997f2f9985fcfe1a800",
+    "url": "/burger-builder-deployment/static/css/4.b4c28651.chunk.css"
+  },
+  {
+    "revision": "e494074fdff8ed13627d",
+    "url": "/burger-builder-deployment/static/css/main.e9f3a487.chunk.css"
+  },
+  {
+    "revision": "1c4af3e69d544d8fdcee",
+    "url": "/burger-builder-deployment/static/js/runtime~main.4922e6f4.js"
+  },
+  {
+    "revision": "56d3df5a9d2a05dfa430",
+    "url": "/burger-builder-deployment/static/js/2.fa016389.chunk.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-builder-deployment/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "227071d0affd2a5c6d83",
+    "url": "/burger-builder-deployment/static/js/3.a2ffd715.chunk.js"
+  },
+  {
+    "revision": "e494074fdff8ed13627d",
+    "url": "/burger-builder-deployment/static/js/main.553f65d1.chunk.js"
+  },
+  {
+    "revision": "7997f2f9985fcfe1a800",
+    "url": "/burger-builder-deployment/static/js/4.2779a391.chunk.js"
+  },
+  {
+    "revision": "bb78775113cfd63c712d",
+    "url": "/burger-builder-deployment/static/js/5.5c03252c.chunk.js"
+  },
+  {
+    "revision": "bb78775113cfd63c712d",
+    "url": "/burger-builder-deployment/static/css/5.03aa874a.chunk.css"
+  },
+  {
+    "revision": "227071d0affd2a5c6d83",
+    "url": "/burger-builder-deployment/static/css/3.3d37f730.chunk.css"
+  },
+  {
+    "revision": "b3f46a3f88d420fdb3282c420131f2e8",
+    "url": "/burger-builder-deployment/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/reac-burger-builder/precache-manifest.74f828ef50765c9f224f6cf59f595545.js"
+  "/burger-builder-deployment/precache-manifest.543d922a9d4cc2c7962a14fcee5030bc.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/reac-burger-builder/index.html", {
+workbox.routing.registerNavigationRoute("/burger-builder-deployment/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
